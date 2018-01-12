190dd49d5fccb18991a8455c33f85eb380c7d497
Tim Kelsall <timothy.kelsall@gmail.com>
2018-01-12 09:09:53 +0000

    Added animations

--- plat.s
+++ plat.s
@@ -4,6 +4,8 @@ TILES_LASTX    equ 19
 TILES_Y        equ 15
 TILES_LASTY    equ 14
 
+importbin gfx/c2b.bin 0 256 data.gfx_c2b
+
 ;------------------------------------------------------------------------------
 ; Main program
 ;------------------------------------------------------------------------------
@@ -12,7 +14,8 @@ start:         call sub_ldlvl
                ldi ra, 280
                ldi rb, 33
                spr 0x1008
-loop:          ldi r1, TILES_LASTY
+loop:          cls
+               ldi r1, TILES_LASTY
 .loop_y:       ldi r0, TILES_LASTX
 .loop_x:       mov r2, r1
                muli r2, TILES_X
@@ -36,15 +39,15 @@ loop:          ldi r1, TILES_LASTY
                call sub_input
                call sub_mvplyr
 
-               spr 0x0804
-               drw ra, rb, data.plyr
-               ;mov r0, rb
-               ;addi r0, 8
-               ;drw ra, r0, data.highl 
-               spr 0x1008
+               call sub_drwplyr
 
 draw_end:      vblnk
-               cls
+               flip 0,0
+               ldm r0, data.v_anim_c
+               addi r0, 1
+               stm r0, data.v_anim_c
+               ldi r0, 0
+               stm r0, data.v_hmov
                jmp loop
 
 spin:          vblnk
@@ -91,6 +94,9 @@ sub_mvleft:    mov r0, ra
                tsti r0, 1
                jnz .sub_mvleft_Z
 .break:        subi ra, 2
+               ldi r0, 1
+               stm r0, data.v_lor
+               stm r0, data.v_hmov
 .sub_mvleft_Z: ret
 
 sub_mvright:   mov r0, ra
@@ -104,7 +110,36 @@ sub_mvright:   mov r0, ra
                add ra, r0
                jmp .sub_mvright_Z
 .sub_mvright_A: addi ra, 2
+               ldi r0, 2
+               stm r0, data.v_lor
+               ldi r0, 1
+               stm r0, data.v_hmov
 .sub_mvright_Z: ret
+
+sub_drwplyr:   ;spr 0x0804
+               mov r0, ra
+               subi r0, 4
+               mov r1, rb
+               subi r1, 8
+               ldi r2, data.gfx_c2b
+               ldm r3, data.v_lor
+               cmpi r3, 1
+               jnz .sub_drwplyr_A
+               flip 1, 0
+               jmp .sub_drwplyr_B
+.sub_drwplyr_A: cmpi r3, 2
+               jnz .sub_drwplyr_B
+               flip 0,0
+.sub_drwplyr_B: ldm r3, data.v_hmov
+               cmpi r3, 0
+               jz .sub_drwplyr_Z
+               ldm r3, data.v_anim_c
+               andi r3, 0x8
+               shl r3, 4
+               add r2, r3
+.sub_drwplyr_Z: drw r0, r1, r2
+               ;spr 0x1008
+               ret
 
 ;------------------------------------------------------------------------------
 ; Return remaining pixels to next 8-aligned y-coordinate
@@ -237,3 +272,6 @@ data.highl:    db 0x33,0x33,0x33,0x33
                db 0x33,0x33,0x33,0x33
 
 data.v_jump:   dw 0
+data.v_lor:    dw 0
+data.v_hmov:   dw 0
+data.v_anim_c: dw 0
